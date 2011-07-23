b98b0a824a6f87e7ec7f5aaccee500a07ffac8e6
Kevin Gorski <kevin.gorski@gmail.com>
2011-07-22 23:12:00 -0600

    Stopped including Color twice in development build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+rm deployable/CYRIN.dev.js
 echo "Combining JavaScript"
 cat lib/color-0.3.0.js CYRIN.TextTreeNode.js CYRIN.Analysis.js CYRIN.js >> deployable/CYRIN.dev.js
 echo "Compressing JavaScript"
 java -jar /usr/local/bin/yuicompressor-2.4.2.jar --line-break 500 -o deployable/CYRIN.min.js --type js deployable/CYRIN.dev.js
 echo "Adding minified files"
-git add deployable/CYRIN.min.js
+git add deployable/CYRIN.min.js deployable/CYRIN.dev.js
